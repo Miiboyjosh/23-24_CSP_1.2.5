8051d24aced9529fb4faabbd15b4dae62342e833
Joshua <joshua.jean@vtsd.com>
2024-01-09 07:58:19 -0500

    Beat Game

--- beat_game_project.py
+++ beat_game_project.py
@@ -1,2 +1,24 @@
+# --Import Turtle--
 import turtle as trtl
 
+# --SIV Turtles--
+track_1 = trtl.Turtle()
+track_2 = trtl.Turtle()
+track_3 = trtl.Turtle()
+track_4 = trtl.Turtle()
+# -- Screen Setup --
+wn = trtl.Screen()
+wn.setup(width=1.0, height=1.0)
+# --Game Config--
+track_1.circle(100)
+
+
+
+# pt 1 - Get the game functioning and playable
+
+# pt 2 - Store and display scores using list
+
+# pt 3 - display highscores by using parameters
+
+wn.bgpic("background.gif")
+wn.mainloop()
