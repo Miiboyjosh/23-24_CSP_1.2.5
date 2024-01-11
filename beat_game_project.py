f4cb1734504d953594fffdb24f2e51b31062b428
Joshua <joshua.jean@vtsd.com>
2024-01-11 09:24:31 -0500

    Beat Game

--- beat_game_project.py
+++ beat_game_project.py
@@ -7,10 +7,32 @@ track_2 = trtl.Turtle()
 track_3 = trtl.Turtle()
 track_4 = trtl.Turtle()
 # -- Screen Setup --
+ringoffire_image = "ringoffire.gif"
 wn = trtl.Screen()
+wn.addshape(ringoffire_image)
 wn.setup(width=1.0, height=1.0)
-# --Game Config--
-track_1.circle(100)
+wn.tracer(False)
+# --Standard Setup--
+letter_list = ['a', 's', 'k', 'l']
+letter = "a"
+ring_letter_x_offset = 25
+ring_letter_y_offset = 50
+
+
+# -- Functions --
+def drawLetter(active_ring, letter):
+    drawer.penup()
+    drawer.goto(active_ring.xcor() - ring_letter_x_offset, active_ring.ycor() - ring_letter_y_offset)
+
+
+
+def drawRing(active_ring, letter):
+    active_ring.showturtle()
+    active_ring.shape(ringoffire_image)
+    drawLetter(active_ring, letter)
+    wn.update()
+
+
 
 
 
@@ -19,6 +41,10 @@ track_1.circle(100)
 # pt 2 - Store and display scores using list
 
 # pt 3 - display highscores by using parameters
+
+
+# --Function Call--
+drawRing(ring1, A)
 
 wn.bgpic("background.gif")
 wn.mainloop()
